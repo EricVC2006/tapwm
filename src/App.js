5d0584e33213d690aca152446b4dd3730792c584
EricVC2006 <161515492+EricVC2006@users.noreply.github.com>
2024-08-23 19:19:55 +0000

    23-08

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import ListaProdutos from './componentes/ListaProdutos';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 const produtos = [{
   nome : "teste",
@@ -14,6 +17,19 @@ const produtos = [{
 
 
 function App() {
+  const [produtos,setProdutos] = useState([]);
+  useEffect(() =>{ carregaPodutos,();
+
+   }, []),
+  function carregaProdutos(){
+
+    axios.get('htttps://app-api-tapwm.orderer.com/api/produtos')
+    .then(res=>(
+      setProdutos (res.data);
+      console.log(produtos);
+    ));
+  }
+
   return (
  <div>
     <h1>Lista Produtos</h1>
